5fdf847c76203a04de55e26b6c7acf5143561de9
rex <373045167@qq.com>
2018-06-22 11:03:25 +0800

    kafka启动说明

--- scdemo-config-client/src/main/java/com/scdemo/config/client/ConfigClientApplication.java
+++ scdemo-config-client/src/main/java/com/scdemo/config/client/ConfigClientApplication.java
@@ -25,5 +25,26 @@ public class ConfigClientApplication {
 		SpringApplication.run(ConfigClientApplication.class, args);
 		
 	}
-
 }
+
+/**
+启动kafka
+1、首先启动zk
+  1.1 cd /usr/local/Cellar/zookeeper/
+  1.2 ls
+  1.3 cd 3.4.12/
+  1.4 cd bin
+  1.5  zkserver start   ----启动zk
+  
+2、再启动kafka
+    2.1 cd /usr/local/Cellar/kafka/  ---到kafka安装目录
+    2.2 ls      ----查看kafka版本 比如我到是 1.1.0
+    2.3 cd 1.1.0
+    2.4 cd bin   
+    2.5 ./kafka-server-start /usr/local/etc/kafka/server.properties  
+3、启动生产进程（testTopic为主题名称） 需要先进入kafka/bin 下面
+./kafka-console-producer --broker-list localhost:9092 --topic testTopic  
+4、启动消费进程  需要先进入kafka/bin 下面
+./kafka-console-consumer --bootstrap-server localhost:9092 --topic testTopic --from-beginning
+ */
+
